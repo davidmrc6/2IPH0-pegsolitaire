167cb01b0c1068540480e212854e7fc324a0fab4
Ryan Sindic <r.sindic@student.tue.nl>
2024-10-07 21:44:44 +0200

    Implemented Exercise 4

--- src/PegSolitaire.hs
+++ src/PegSolitaire.hs
@@ -62,13 +62,20 @@ stringToPegs = map f
 --
 -- === __Examples__
 -- >>> isWinning [Peg, Empty, Empty, Empty, Peg, Peg]
--- False
+-- WAS WAS False
+-- WAS NOW C:\Users\20231620\AppData\Local\Temp\extE70F: withFile: resource busy (file is locked)
+-- NOW False
 --
 -- >>> isWinning [Empty, Empty, Peg, Peg]
--- False
+-- WAS WAS False
+-- WAS NOW False
+-- WAS NOW False
+-- NOW False
 --
--- >>> isWinning [Empty, Peg, Empty, Empty, Empty]
--- True
+-- WAS True
+-- NOW True
+-- WAS True
+-- NOW True
 --
 isWinning :: Pegs -> Bool
 isWinning pegs = (length . filter (== Peg)) pegs == 1
@@ -111,6 +118,51 @@ tryRight _ = Nothing
 tryLeft :: Zipper a -> Maybe (Zipper a)
 tryLeft (Zipper (l:ls) focus right) = Just (Zipper ls l (focus:right))
 tryLeft _ = Nothing
+
+-- | Generates all possible game states of length n with exactly one empty position.
+--
+-- This function generates all possible game states where n-1 positions are filled with pegs and one position is empty.
+--
+-- === __Examples__
+--
+-- >>> generateLinearStates 2
+-- [[Empty,Peg],[Peg,Empty]]
+--
+-- >>> generateLinearStates 3
+-- [[Empty,Peg,Peg],[Peg,Empty,Peg],[Peg,Peg,Empty]]
+--
+generateLinearStates :: Int -> [Pegs]
+generateLinearStates n = unfoldr f 0
+  where
+    f :: Int -> Maybe (Pegs, Int)
+    f i
+      | i >= n = Nothing
+      | otherwise = Just (pegsList i, i+1)
+    pegsList :: Int -> Pegs
+    pegsList i = [if j == i then Empty else Peg | j <- [0..n-1]]
+
+-- | Generates all possible game states up to length n.
+--
+-- This function generates all possible game states of lengths from 1 up to n, where each position can be either a peg or empty.
+--
+-- === __Examples__
+--
+-- >>> generateStates 2
+-- [[Empty],[Peg],[Empty,Empty],[Peg,Empty],[Empty,Peg],[Peg,Peg]]
+--
+generateStates :: Int -> [Pegs]
+generateStates n = concatMap generateStatesOfLength [1..n]
+  where
+    generateStatesOfLength :: Int -> [Pegs]
+    generateStatesOfLength k = unfoldr f 0
+      where
+        maxNum = 2^k - 1
+        f :: Int -> Maybe (Pegs, Int)
+        f i
+          | i > maxNum = Nothing
+          | otherwise = Just (intToPegs i k, i+1)
+    intToPegs :: Int -> Int -> Pegs
+    intToPegs i k = [if testBit i j then Peg else Empty | j <- [0..k-1]]
 
 makeMoves = error "Implement, document, and test this function"
 unfoldT = error "Implement, document, and test this function"
